4542e86c0c0af198ec2862b51a5e3ade50a981a1
gretanorv <gretanorv@gmail.com>
2021-02-06 14:34:47 +0200

    route fix

--- database/migrations/2021_02_04_050741_create_employees_table.php
+++ database/migrations/2021_02_04_050741_create_employees_table.php
@@ -1,24 +1,24 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeesTable extends Migration
-{
-    public function up()
+    class CreateEmployeesTable extends Migration
     {
-        Schema::create('employees', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 30);
-            $table->string('surname', 30);
-            $table->timestamps();
-            $table->foreignId('project_id')->references('id')->on('projects');
-        });
-    }
+        public function up()
+        {
+            Schema::create('employees', function (Blueprint $table) {
+                $table->id();
+                $table->string('name', 30);
+                $table->string('surname', 30);
+                $table->timestamps();
+                $table->foreignId('project_id')->references('id')->on('projects')->onDelete('cascade');
+            });
+        }
 
-    public function down()
-    {
-        Schema::dropIfExists('employees');
+        public function down()
+        {
+            Schema::dropIfExists('employees');
+        }
     }
-}
